9d714137399375f7cefabc53591d88dae5c88475
paulinetmvo <94937311+paulinetmvo@users.noreply.github.com>
2022-07-22 12:36:19 +0200

    settings secret django-settings unterstrich zu bindestrich

--- stars/stars/settings.py
+++ stars/stars/settings.py
@@ -52,7 +52,7 @@ elif os.getenv("GOOGLE_CLOUD_PROJECT", None):
     project_id = "1033113497693"
 
     client = secretmanager.SecretManagerServiceClient()
-    settings_name = os.getenv("SETTINGS_NAME", "django_settings")
+    settings_name = os.getenv("SETTINGS_NAME", "django-settings")
     name = f"projects/{project_id}/secrets/{settings_name}/versions/latest"
     payload = client.access_secret_version(name=name).payload.data.decode(
         "UTF-8"
