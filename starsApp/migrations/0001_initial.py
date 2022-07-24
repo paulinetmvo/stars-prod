876449c52f2729ef68a5da27aa97b30c6293a9c4
paulinetmvo <paulinetmvo@gmail.com>
2022-07-24 17:56:54 +0200

    alle allowed hosts

--- starsApp/migrations/0001_initial.py
+++ starsApp/migrations/0001_initial.py
@@ -18,7 +18,7 @@ from google.cloud import secretmanager
 def createsuperuser(apps: StateApps, schema_editor: DatabaseSchemaEditor) -> None:
     """
     Dynamically create an admin user as part of a migration
-    Password is pulled from Secret Manger (previously created as part of tutorial)
+    Password is pulled from Secret Manger
     """
     client = secretmanager.SecretManagerServiceClient()
 
