34864da6f96dde34f1fe16d8953b9d137c5f92a6
paulinetmvo <paulinetmvo@gmail.com>
2022-07-24 16:52:53 +0200

    superuser password bindestrich statt unterstrich

--- starsApp/migrations/0001_initial.py
+++ starsApp/migrations/0001_initial.py
@@ -20,21 +20,17 @@ def createsuperuser(apps: StateApps, schema_editor: DatabaseSchemaEditor) -> Non
     Dynamically create an admin user as part of a migration
     Password is pulled from Secret Manger (previously created as part of tutorial)
     """
-    if os.getenv("TRAMPOLINE_CI", None):
-        # We are in CI, so just create a placeholder user for unit testing.
-        admin_password = "test"
-    else:
-        client = secretmanager.SecretManagerServiceClient()
+    client = secretmanager.SecretManagerServiceClient()
 
-        # Get project value for identifying current context
-        _, project = google.auth.default()
+    # Get project value for identifying current context
+    _, project = google.auth.default()
 
-        # Retrieve the previously stored admin password
-        PASSWORD_NAME = os.environ.get("PASSWORD_NAME", "superuser_password")
-        name = f"projects/{project}/secrets/{PASSWORD_NAME}/versions/latest"
-        admin_password = client.access_secret_version(name=name).payload.data.decode(
-            "UTF-8"
-        )
+    # Retrieve the previously stored admin password
+    PASSWORD_NAME = os.environ.get("PASSWORD_NAME", "superuser-password")
+    name = f"projects/{project}/secrets/{PASSWORD_NAME}/versions/latest"
+    admin_password = client.access_secret_version(name=name).payload.data.decode(
+        "UTF-8"
+    )
 
     # Create a new user using acquired password, stripping any accidentally stored newline characters
     User.objects.create_superuser("admin", password=admin_password.strip())
